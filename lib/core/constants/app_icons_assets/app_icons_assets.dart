d86dbefd169f122b5484a0e9aa4d8dba7f1bad90
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2026-01-06 14:22:47 +0500

    create a class for icon strings

---
+++ lib/core/constants/app_icons_assets/app_icons_assets.dart
@@ -0,0 +1,11 @@
+
+
+class AppIconsAssets {
+
+   static const String baseUrl = 'assets/icons';
+
+   static const String visibilityOffIcon = '$baseUrl/visibility_off.png';
+   static const String googleIcon = '$baseUrl/google_icon.png';
+   static const String facebookIcon = '$baseUrl/facebook_icon.png';
+
+}
