0e531c9080c66f5d5f47334d3f1d3ca20d4fc13e
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2026-01-06 14:21:52 +0500

    manage app exports/barrels

--- lib/app_barrels.dart
+++ lib/app_barrels.dart
@@ -1,7 +1,21 @@
 export 'package:flutter/material.dart';
-export 'package:expense_tracker_app/core/constants/app_images/app_images.dart';
-export 'package:expense_tracker_app/core/constants/app_strings/app_strings.dart';
+export 'package:expense_tracker_app/core/constants/app_images_assets/app_images.dart';
+export 'package:expense_tracker_app/core/constants/app_strings_assets/app_strings.dart';
 export 'package:expense_tracker_app/core/utils/responsive/responsive.dart';
 export 'package:expense_tracker_app/shared/widgets/custom_button.dart';
 export 'package:expense_tracker_app/core/constants/app_colors/app_colors.dart';
 export 'package:expense_tracker_app/features/auth/presentation/screens/login_screen/login_screen.dart';
+export 'package:expense_tracker_app/shared/widgets/custom_text_field.dart';
+export 'package:expense_tracker_app/core/constants/app_icons_assets/app_icons_assets.dart';
+
+
+
+
+
+
+
+
+
+
+
+
